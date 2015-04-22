d89a1957afe56f0c4ce4be0aaef01ed2ad66678a
Mauricio Lucon <mlucon@Favela-do-Mau.local>
2015-04-22 15:43:06 -0300

    ButtonbsDesignFinal

---
+++ ButtonsDesignVol2/DesignButtonViewFive.swift
@@ -0,0 +1,52 @@
+//
+//  DesignButtonViewFive.swift
+//  ButtonsDesign
+//
+//  Created by Mauricio Lucon on 4/15/15.
+//  Copyright (c) 2015 AppFact. All rights reserved.
+//
+
+import UIKit
+
+class DesignButtonViewFive: UIButton {
+    
+    // Variáveis
+    let onImage = UIImage(named: "onDesign05.png")
+    let offImage = UIImage(named: "offDesign05.png")
+    
+    // Propriedade Bool
+    var Pressed:Bool = false {
+        didSet {
+            if Pressed == true {
+                self.setImage(onImage, forState: .Normal)
+                NSLog("Ligado")
+                
+            } else {
+                self.setImage(offImage, forState: .Normal)
+                NSLog("Desligado")
+            }
+        }
+    }
+    
+    
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action: "BotaoClicado:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.Pressed = false
+    }
+    
+    
+    
+    func BotaoClicado(sender:UIButton) {
+        if(sender == self){
+            if Pressed == true {
+                Pressed = false
+            } else{
+                Pressed = true
+            }
+        }
+    }
+    
+    
+   
+}
